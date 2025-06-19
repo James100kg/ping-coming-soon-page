88fad443a52b1b2ba1a60720533d76764d0c8fa9
James100kg <peterjames0319@gmail.com>
2025-06-19 15:53:40 -0400

    Mobile Layout

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./images/logo.svg"
+import graph from "./images/illustration-dashboard.png"
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { MobileLayout } from "./MobileLayout";
+import { ParentWrapper } from "./ParentWrapper";
+
+
+
+
+function App() {
+    return (
+      <ParentWrapper />
+    )
+}
+
+export default App;
---
+++ src/ParentWrapper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { MobileLayout } from "./MobileLayout";
+
+
+export function ParentWrapper() {
+    return (
+    <MobileLayout />
+)
+}
