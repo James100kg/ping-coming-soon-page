473bcc2a6e42dc7905b1165fd0adfc7064d5842d
Peter <james0319@gmail.com>
2025-06-20 01:30:27 -0400

    Initial commit

---
+++ src/DesktopLayout.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "./images/logo.svg"
+import graph from "./images/illustration-dashboard.png"
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+export function DesktopLayout({ email, setEmail, emailError, handleSubmit }) {
+    return (
+          <div className="font-poppins font-light max-w-lg mx-auto content-wrapper p-6 flex flex-col justify-center items-center">
+              <img src={logo} className="pt-20 mb-6" />  
+              <h1 className="pt-10 mb-8 text-[35px] text-gray-400">We are launching <span className="text-black font-bold">soon!</span></h1>
+              <h2 className="text-[18px] mb-10">Subscribe and get notified</h2>
+              <div className="flex gap-4 justify-center max-w-xl mx-auto w-full">
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Your email address..." className="h-14 flex-1 mb-4 px-8 py-4 border rounded-full border-solid border-blue-200 placeholder:text-lg placeholder:text-blue-200 placeholder-opacity-60"></input>
+              <button onClick={handleSubmit} className="h-14 px-10 mb-20 rounded-full bg-blue-500 text-white font-semibold shadow-xl shadow-blue-200/50">Notify Me</button>
+              <div>
+                {emailError && <p className="text-red-500">{emailError}</p>}
+              </div>
+              </div>
+              <img src={graph} className="mb-20"/>
+              <div className="icons flex gap-6 mt-20 mb-6">
+                <div className="p-3 border rounded-full border-gray-300 text-blue-500">
+                <FaFacebookF className="text-xl" />
+                </div>
+                <div className="p-3 border rounded-full border-gray-300 text-blue-500">
+                <FaTwitter className="text-blue-500 text-xl" />
+                </div>
+                <div className="p-3 border rounded-full border-gray-300 text-blue-500">
+                <FaInstagram className="text-blue-500 text-xl" />
+                </div>
+              </div>
+              <p className="text-gray-400 text-sm">© Copyright Ping. All rights reserved.</p>
+          </div>
+        )
+}
--- src/ParentWrapper.jsx
+++ src/ParentWrapper.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import { React, useState } from "react";
 import { MobileLayout } from "./MobileLayout";
+import { DesktopLayout } from "./DesktopLayout";
 
 
 export function ParentWrapper() {
+    const [email, setEmail] = useState("")
+    const [emailError, setEmailError] = useState("")
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        
+        const trimmedEmail = email.trim()
+
+         if (trimmedEmail === "") {
+        setEmailError("Email is required")
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(trimmedEmail)) {
+            setEmailError("Email is invalid")
+        } else {
+            setEmailError("")
+        }
+        
+    }
+    
     return (
+    <>
+    <div className="block md:hidden">
     <MobileLayout />
+    </div>
+    <div className="hidden md:block">
+    <DesktopLayout
+    email={email}
+    emailError={emailError}
+    setEmail={setEmail}
+    handleSubmit={handleSubmit} />
+    </div>
+    </>
 )
 }
